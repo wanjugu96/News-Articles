0a1b98990ad6661192351990d36cbcef337155f9
wanjugu mung'au <wanjugu.mung'au@strathmore.edu>
2021-09-14 10:29:27 +0300

    adding articles unit test

--- tests/test_article.py
+++ tests/test_article.py
@@ -10,5 +10,19 @@ class ArticleTest(unittest.TestCase):
         Set up method to run before any tes
         '''
 
+#title,description,url,urlToImage,publishedAt,source ,author
+        self.new_article=Article("Dancing stars","who can dance","example.com","exampleimage.com","12-3-3","BBC","Mary")
 
-        self.new_article
+    def test_instance(self):
+        self.assertTrue(isinstance (self.new_article,Article))
+        self.assertEqual(self.new_article.title,"Dancing Stars")
+        self.assertEqual(self.new_article.description,"who can dance")
+        self.assertEqual(self.new_article.url,"example.com")
+        self.assertEqual(self.new_article.urlToImage,"exampleimage.com")
+        self.assertEqual(self.new_article.publishedAt,"12-3-3")
+        self.assertEqual(self.new_article.source,"BBC","Mary")
+
+
+     
+
+
